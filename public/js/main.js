af11bd1f0bfa50ff568cfb059b425bf26f2c6bbc
moshe cohen <moshe22cohen@gmail.com>
2019-03-17 13:23:36 +0200

    buid gui

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,7 @@
-/// <reference path="../src/js/socket.io.js" />
+/// <reference path="../js/socket.io.js" />
+
+//Make connection 
+var port=4000;
+var host="http://localhost:";
+var socket= io.connect(host+port);
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var socket = require("socket.io");
 var colors= require("colors");
 //App setup
 
@@ -12,3 +13,13 @@ console.log("listening to requests on port 4000".yellow);
 //Static files
 
 app.use(express.static("public"));
+
+
+//Socket setup
+var io= socket(server);
+io.on("connection",(socket)=>{
+    console.log("made socket connection.".yellow,socket.id);
+});
+
+
+
